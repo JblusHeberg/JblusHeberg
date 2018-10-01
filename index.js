0f0787ae524186898cbabf9603d325aafdf27c99
JblusHeberg <43757446+JblusHeberg@users.noreply.github.com>
2018-10-01 18:51:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client()
 
 var prefix = ">>";
 
-client.login("NDkyMDI5MjcyMzY2NjQ1MjQ5.DoQwjw.i100wV-qrRNf6otne93vFwXMKSY");
+client.login(process.env.TOKEN);
 
 client.on("ready", () => {
 	console.log("[JblusCode] You have Enable");
